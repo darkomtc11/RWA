b803d24cc8a30d820df199d44b158809ebb55bcb
dawrko <darkomtc11@gmail.com>
2019-04-28 21:39:31 +0200

    IP adresa json-servera promenjena

--- projekat1/web-client/src/environments.ts
+++ projekat1/web-client/src/environments.ts
@@ -1,5 +1,5 @@
 export const environments = {
-  serverApiUrl: 'http://24.135.29.89:3000',
+  serverApiUrl: 'http://localhost:3000',
   usersResourceUrl: 'users',
   leaguesResourceUrl: 'leagues',
   matchesResourceUrl: 'matches',
